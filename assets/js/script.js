3c8b21d3826704c3ce5952d3d4e1b6a78fb44728
María Fernanda Ramírez <mframirezfuentes@gmail.com>
2021-06-03 15:51:06 -0400

    funcion IIFE funcionando además con la entrega de parametros

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,12 @@ class Multimedia {
         let _url = url;
         this.getUrl = () => _url;
         this.setUrl = (nueva_url) => _url = nueva_url;
-
+    }
+    get url() {
+        return this.getUrl();
+    }
+    set url(nueva_url) {
+        this.setUrl(nueva_url)
     }
     setInicio() {
         return `Este método es para realizar un cambio en la URL del video`
@@ -16,34 +21,49 @@ class Reproductor extends Multimedia {
         this.getId = () => _id;
         this.setId = (nuevoId) => _id = nuevoId
     }
-    playMultimedia(url, id) {
-        mostrarVideo.videoMusica(url, id);
-        mostrarVideo.videoPelicula(url, id);
-        mostrarVideo.videoSerie(url, id);
+    get id() {
+        return this.getId;
     }
+    set id(nueva_id) {
+        this.getId(nueva_id)
+    }
+
+    setInicio(tiempo1,tiempo2,tiempo3) {
+        document.getElementById('musica').innerHTML = `<iframe width="560" height="315" src="${urlMusica}?start=${tiempo1}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+       document.getElementById('peliculas').innerHTML = `<iframe width="560" height="315" src="${urlPelicula}?start=${tiempo2}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+       document.getElementById('series').innerHTML = `<iframe width="560" height="315" src="${urlSerie}?start=${tiempo3}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+  
+    }
+    playMultimedia() {
+        mostrarVideo.videoMusica(this.url, this.id());
+        mostrarVideo.videoPelicula(this.url, this.id());
+        mostrarVideo.videoSerie(this.url, this.id());
+    }
+
+
 }
 
 //Modulo que nos servirá para mostrar el patrón
 const mostrarVideo = (() => {
-    let musicaV = () => {
+    let musicaV = (urlMusica, idMusica) => {
         idMusica.innerHTML = `<iframe width="560" height="315" src="${urlMusica}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
     };
-    let peliculaV = () => {
+    let peliculaV = (urlPelicula, idPelicula) => {
         idPelicula.innerHTML = `<iframe width="560" height="315" src="${urlPelicula}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
     };
-    let serieV = () => {
+    let serieV = (urlSerie, idSerie) => {
         idSerie.innerHTML = `<iframe width="560" height="315" src="${urlSerie}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
     };
 
     return {
         videoMusica: () => {
-            musicaV()
+            musicaV(urlMusica, idMusica)
         },
         videoPelicula: () => {
-            peliculaV()
+            peliculaV(urlPelicula, idPelicula)
         },
         videoSerie: () => {
-            serieV()
+            serieV(urlSerie, idSerie)
         }
     }
 })();
@@ -61,6 +81,9 @@ let pelicula = new Reproductor(urlPelicula, idPelicula)
 let serie = new Reproductor(urlSerie, idSerie)
 
 //llamada al método
-musica.playMultimedia(urlMusica, idMusica);
-pelicula.playMultimedia(urlPelicula, idPelicula);
-serie.playMultimedia(urlSerie, idSerie);
+musica.playMultimedia();
+pelicula.playMultimedia();
+serie.playMultimedia();
+musica.setInicio(10,15,20)
+pelicula.setInicio(10,15,20)
+serie.setInicio(10,15,20)
